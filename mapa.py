bf73dad5907c3b8404f95c573962c140c95ce60b
jjaszczak <83343745+jjaszczak@users.noreply.github.com>
2021-06-01 19:10:34 +0200

    Add files via upload

---
+++ mapa.py
@@ -0,0 +1,18 @@
+from pygame import image, Color
+
+moveimage = image.load() #tutaj wstawimy plik
+
+def check_move_point(gracz):
+    move_x, move_y = 0, 0
+    if gracz.keys_active["right"]:
+        move_x = 1
+    if gracz.keys_active["up"]:
+        move_y = -1
+    if gracz.keys_active["left"]:
+        move_x = -1
+    if gracz.keys_active["down"]:
+        move_x = 1
+
+    if moveimage.get_at((int(gracz.x+move_x), int(gracz)))  != Color("black"):
+        return False
+    return True
